5ece8849a90712082868beafc6b6a0c0cd55d729
Gotchun <qkreogo1@naver.com>
2024-09-26 17:48:55 +0900

    .

--- src/C17/C04ExceptionMain.java
+++ src/C17/C04ExceptionMain.java
@@ -4,10 +4,11 @@ public class C04ExceptionMain {
 
 	public static void main(String[] args) {
 
+
+
 		try {
 		String str=null;
 		str.toString();		//null  예외발생
-
 
 		int arr[] = {10 , 20 , 30};
 		arr[5] = 100;					//Bound Exception 발생	//배열길이보다 큼
@@ -15,15 +16,10 @@ public class C04ExceptionMain {
 
 		Animal animal = new Dog();
 		Cat yummi = (Cat)animal;		//Cast Exception 발생
-		}catch() {
-
-		}catch() {
-
-		}catch() {
-
-		}catch() {
-
 		}
 
+		catch(Exception e) {
+			System.out.println("통합예외발생" + e);		//여기서 e 는 오류문출력임
+		}
 	}
 }
